129acaa282227cf90e833d394804fb52e6ad1694
Soyl <Soylgr@yandex.ru>
2026-01-25 07:38:21 +0300

    new/fix: Добавления перехода между настройками и домом

--- app/src/main/java/com/example/helppraepostor/MainActivity.java
+++ app/src/main/java/com/example/helppraepostor/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.helppraepostor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+    private int numberTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText editText = findViewById(R.id.NumberTable);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    numberTable = Integer.parseInt(editable.toString().trim());
+                }catch (NumberFormatException e) {
+                    numberTable = 0;
+                }
+            }
+        });
+    }
+
+    public void goToSetting(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+    public void goToTable(View view) {
+        if(numberTable != 0){
+            Intent intent = new Intent(this, TableActivity.class);
+            startActivity(intent);
+        }else {
+            Snackbar.make(view,"Введите число полей", Snackbar.LENGTH_SHORT).setBackgroundTint(0XFF555553).show();
+        }
     }
 }
